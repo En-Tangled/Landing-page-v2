f1d88e4cc777095bbfca3cbbbdd80f0b9bda9006
0xankit <ankit.kr554@gmail.com>
2024-02-02 03:18:59 +0530

    feat: add waitlist url

--- components/hero.js
+++ components/hero.js
@@ -22,7 +22,7 @@ const Hero = () => {
             Join the limited waitlist now!
             </p>
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
-              <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">
+              <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"   onClick={() => { window.location.href = 'https://twitter.com/ciberexplosion/status/1753170876177199114'; }}>
                 Register for the waitlist
               </button>
             </div>
